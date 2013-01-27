f355245c2b6866745208421c96f8a05f419a87ce
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-01-28 00:33:37 +0200

    testing if a string is a rotation of another

---
+++ src/main/java/learning/ctci/ArraysAndStrings/RotationTester.java
@@ -0,0 +1,8 @@
+package learning.ctci.ArraysAndStrings;
+
+/**
+ * @author aburghelea
+ * @since 1/28/13 - 12:15 AM
+ */
+public class RotationTester {
+}
---
+++ src/test/java/learning/ctci/ArraysAndStrings/RotationTesterTest.java
@@ -0,0 +1,8 @@
+package learning.ctci.ArraysAndStrings;
+
+/**
+ * @author aburghelea
+ * @since 1/28/13 - 12:21 AM
+ */
+public class RotationTesterTest {
+}
