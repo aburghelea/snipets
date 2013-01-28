75869cc16da08ac8f5a8448c3100ef0e99678351
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-01-28 13:32:16 +0200

    testing if a string is a rotation

--- src/main/java/learning/ctci/ArraysAndStrings/RotationTester.java
+++ src/main/java/learning/ctci/ArraysAndStrings/RotationTester.java
@@ -5,4 +5,9 @@ package learning.ctci.ArraysAndStrings;
  * @since 1/28/13 - 12:15 AM
  */
 public class RotationTester {
+
+    public static boolean isRotation(String original, String candidate){
+        original += original;
+        return original.contains(candidate);
+    }
 }
--- src/test/java/learning/ctci/ArraysAndStrings/RotationTesterTest.java
+++ src/test/java/learning/ctci/ArraysAndStrings/RotationTesterTest.java
@@ -1,8 +1,47 @@
 package learning.ctci.ArraysAndStrings;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 /**
  * @author aburghelea
  * @since 1/28/13 - 12:21 AM
  */
 public class RotationTesterTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testNullStrings() throws Exception {
+        RotationTester.isRotation(null, null);
+        fail("Should have failed");
+    }
+
+    @Test
+    public void testIdenticalWords() throws Exception {
+        assertTrue(RotationTester.isRotation("gigel", "gigel"));
+    }
+
+    @Test
+    public void testOnLeftRotation() throws Exception {
+        assertTrue(RotationTester.isRotation("gigel", "igelg"));
+    }
+
+    @Test
+    public void testRightRotation() throws Exception {
+        assertTrue(RotationTester.isRotation("gigel", "lgigel"));
+    }
+
+    @Test
+    public void testRandomStrings() throws Exception {
+        assertFalse(RotationTester.isRotation("gigsadasasdel", "lgig2el"));
+
+    }
+
+    @Test
+    public void testWatterBottle() throws Exception {
+        assertTrue(RotationTester.isRotation("waterbottle", "erbottlewat"));
+
+    }
 }
