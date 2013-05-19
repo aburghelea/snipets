a8b44b7a64641f2f711117eb3fd429dcc16a3492
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-05-19 23:50:27 +0300

    Added php SSE test

---
+++ src/test/java/learning/misc/GenericsTest.java
@@ -0,0 +1,11 @@
+package learning.misc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iceman
+ * Date: 4/21/13
+ * Time: 3:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GenericsTest {
+}
