20f7c3b384a77be1221b16225691c12ceb0e4ea6
Alexandru Burghelea <iceman.ftg@gmail.com>
2013-05-19 23:52:24 +0300

    Add Generics Add test

--- src/test/java/learning/misc/GenericsTest.java
+++ src/test/java/learning/misc/GenericsTest.java
@@ -1,11 +1,35 @@
 package learning.misc;
 
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created with IntelliJ IDEA.
- * User: iceman
- * Date: 4/21/13
- * Time: 3:30 PM
- * To change this template use File | Settings | File Templates.
+ * @author aburghelea
+ * @since 4/21/13 - 3:30 PM
  */
 public class GenericsTest {
+
+    @Test
+    public void testGenericAddDivegingSubclass() throws Exception {
+        BigDecimal bigDecimalOne = BigDecimal.ONE;
+        List<Serializable> list = new ArrayList<>();
+        String stringTwo = "2";
+        list.add(bigDecimalOne);
+        list.add(stringTwo);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+    }
+
+    @Test
+    public void addSubclass() throws Exception {
+        BigDecimal bigDecimalOne = BigDecimal.ONE;
+        List<Number> list = new ArrayList<>();
+        list.add(bigDecimalOne);
+        System.out.println(list.get(0));
+    }
+
 }
